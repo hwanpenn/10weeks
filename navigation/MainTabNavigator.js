d1c39fc8732cd14a6196b66fa553e5d4a09a45c8
hwanpenn <hwanpenn@gmail.com>
2018-07-04 09:09:51 +0800

    清空
    清空

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import LinksScreen from '../screens/LinksScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+import Login from "../screens/Login";
+import Details from "../screens/Details";
+import Home from "../screens/Home";
+import Regist from "../screens/Regist";
+import LoginAndRegist from "../screens/LoginAndRegist";
+
+// const HomeStack = createStackNavigator({
+//     Home: HomeScreen,
+// });
+const HomeStack = createStackNavigator(
+    {
+        // Home: HomeScreen,
+        Home: Home,
+        Details: Details,
+        LoginAndRegist: LoginAndRegist,
+        Login: Login,
+        Regist: Regist,
+    },
+    {
+        initialRouteName: 'Home',
+    }
+);
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  ),
+};
+
+const LinksStack = createStackNavigator({
+  Links: LinksScreen,
+});
+
+LinksStack.navigationOptions = {
+  tabBarLabel: 'Links',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? `ios-link${focused ? '' : '-outline'}` : 'md-link'}
+    />
+  ),
+};
+
+const SettingsStack = createStackNavigator({
+  Settings: SettingsScreen,
+});
+
+SettingsStack.navigationOptions = {
+  tabBarLabel: 'Settings',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options'}
+    />
+  ),
+};
+
+export default createBottomTabNavigator({
+  HomeStack,
+  LinksStack,
+  SettingsStack,
+});
